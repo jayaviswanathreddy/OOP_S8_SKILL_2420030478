5f2b87253315367060fd875982df41f433c1ba11
jayaviswanathreddy <tjvreddy07@gmail.com>
2025-07-30 11:01:58 +0530

    Add files via upload

---
+++ Skill-1/src/studentdashboardmodule.java
@@ -0,0 +1,17 @@
+public class studentdashboardmodule {
+    public static void main(String[] args) {
+        long rollNumber = 2420030478L;
+        String name = "VISWANATH";
+        float marks = 85.75f;
+        char gender = 'M';
+        boolean isPass = true;
+
+        System.out.println("------ Student Summary Slip ------");
+        System.out.println("Roll Number   : " + rollNumber);
+        System.out.println("Name          : " + name);
+        System.out.println("Marks         : " + marks);
+        System.out.println("Gender        : " + gender);
+        System.out.println("Pass Status   : " + (isPass ? "Pass" : "Fail"));
+        System.out.println("----------------------------------");
+    }
+}
